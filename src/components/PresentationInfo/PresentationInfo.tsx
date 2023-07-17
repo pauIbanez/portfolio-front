8d94f1fada8881e2e8a07fb226b4be87d645c3a2
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-17 18:52:48 +0200

    added local font and added name to PresentationInfo

--- src/components/PresentationInfo/PresentationInfo.tsx
+++ src/components/PresentationInfo/PresentationInfo.tsx
@@ -2,9 +2,9 @@ import { styled } from "styled-components";
 
 const Container = styled.div`
   height: 75px;
-  background-color: white;
   display: flex;
   align-items: center;
+  gap: 15px;
 `;
 
 const MyLogo = styled.img`
@@ -12,12 +12,19 @@ const MyLogo = styled.img`
   width: 50px;
 `;
 
-const Text = styled.h3``;
+const Name = styled.h3`
+  color: black;
+  font-size: 16px;
+  font-weight: 700;
+  font-family: "Poppins";
+  margin: 0;
+`;
 
 const PresentationInfo = () => {
   return (
     <Container>
       <MyLogo alt="My logo" src="./media/logo.svg" />
+      <Name>PAU IBÁÑEZ PORTFOLIO</Name>
     </Container>
   );
 };
