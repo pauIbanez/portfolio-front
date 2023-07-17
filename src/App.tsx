bf7d3e9b7e71af50de1140b51a877120a07de814
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-17 20:38:07 +0200

    created the layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Pages from "./data/Pages/Pages";
-import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to={Pages.home.path} />} />
-      <Route
-        path={Pages.home.path}
-        element={
-          <>
-            <Header />
-          </>
-        }
-      />
-      <Route path={Pages.aboutMe.path} element={<p>About me Page</p>} />
-      <Route path={Pages.projects.path} element={<p>Projects Page</p>} />
-      <Route path={Pages.project.path}>
-        <Route path={Pages.project.id} element={<p>Project page</p>} />
-      </Route>
-      <Route path={Pages.curriculum.path} element={<p>CV Page</p>} />
-      <Route path={Pages.contact.path} element={<p>Contact Page</p>} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to={Pages.home.path} />} />
+        <Route path={Pages.home.path} element={<p>Home page</p>} />
+        <Route path={Pages.aboutMe.path} element={<p>About me Page</p>} />
+        <Route path={Pages.projects.path} element={<p>Projects Page</p>} />
+        <Route path={Pages.project.path}>
+          <Route path={Pages.project.id} element={<p>Project page</p>} />
+        </Route>
+        <Route path={Pages.curriculum.path} element={<p>CV Page</p>} />
+        <Route path={Pages.contact.path} element={<p>Contact Page</p>} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import Header from "../Header/Header";
+
+interface Props {
+  children: JSX.Element;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
