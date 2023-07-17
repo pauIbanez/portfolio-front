5f6ffd4d2a296d21391f2b8cde6907da1f66bc00
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-17 19:38:47 +0200

    created the Colors enum

---
+++ src/data/style/Colors.ts
@@ -0,0 +1,11 @@
+enum Colors {
+  main = "#9747FF",
+  textGray = "#252525",
+  lightTextGray = "#757575",
+  backgroundWhite = "#EAEAEA",
+  projectCardBackground = "#D1D1D1",
+  disabledMain = "#9747FF",
+  disabledTextGray = "#757575",
+}
+
+export default Colors;
