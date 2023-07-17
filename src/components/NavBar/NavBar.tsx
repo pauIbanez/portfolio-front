36bb825ebf9d4f3d16ed8400e8fd4655e86b9b60
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-17 19:53:32 +0200

    created the NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,3 +1,31 @@
-const NavBar = () => {};
+import { styled } from "styled-components";
+import Pages from "../../data/Pages/Pages";
+import { Link } from "react-router-dom";
+import Colors from "../../data/style/Colors";
+
+const NavBarContainer = styled.nav`
+  height: 100%;
+  display: flex;
+`;
+const NavItem = styled(Link)`
+  height: 100%;
+  width: 140px;
+  color: ${Colors.textGray};
+  font-size: 16;
+  font-weight: 700;
+
+  &:hover {
+    color: white;
+    background-color: ${Colors.main};
+  }
+`;
+
+const NavBar = () => {
+  const pages = Object.values(Pages).map((page) => (
+    <NavItem to={page.path}>{page.name}</NavItem>
+  ));
+
+  return <NavBarContainer>{pages}</NavBarContainer>;
+};
 
 export default NavBar;
