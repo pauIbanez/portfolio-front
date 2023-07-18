23b683225cf33558bd7c85fd40a1f002d3bb09ae
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-18 17:12:16 +0200

    removed language change test button from PageTitle

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 import ColoredText from "../ColoredText/ColoredText";
-import { useTranslation } from "react-i18next";
-import { useState } from "react";
-import languages from "../../data/languages/languages";
 
 const Container = styled.header`
   width: 100%;
@@ -26,10 +23,6 @@ interface Props {
 }
 
 const PageTitle = ({ title }: Props) => {
-  const [lang, setLang] = useState("en");
-
-  const { i18n } = useTranslation();
-
   const text =
     typeof title === "string" ? (
       <h1>{title}</h1>
@@ -37,21 +30,7 @@ const PageTitle = ({ title }: Props) => {
       <ColoredText textArray={title} styleData={{ isTitle: true }} />
     );
 
-  return (
-    <Container>
-      {text}{" "}
-      <button
-        onClick={() => {
-          const lng = languages[lang === "en" ? 1 : 0];
-          i18n.changeLanguage(lng.code);
-          setLang(lng.code);
-          console.log(lng.code);
-        }}
-      >
-        Change lange
-      </button>{" "}
-    </Container>
-  );
+  return <Container>{text}</Container>;
 };
 
 export default PageTitle;
