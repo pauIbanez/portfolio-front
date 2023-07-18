51733d4665044dca091076d7435328d669623c84
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-18 13:13:17 +0200

    tested the navBar component

---
+++ src/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,43 @@
+import { screen } from "@testing-library/react";
+import NavBar from "./NavBar";
+import { renderInRouter } from "../../setupTests";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({ pathname: "/home" }),
+}));
+
+describe("Given the NavBar component", () => {
+  describe("When it's intanciated in the page home", () => {
+    test("Then it should render the pages", () => {
+      const expectedPages = ["Home", "About me", "Projects", "CV", "Contact"];
+
+      renderInRouter(<NavBar />);
+
+      const foundLinks = screen.getAllByRole("link");
+
+      let foundPages = 0;
+      foundLinks.forEach((link) => {
+        const inPages = expectedPages.includes(link.textContent!);
+        expect(inPages).toBe(true);
+
+        if (inPages) {
+          foundPages += 1;
+        }
+      });
+      expect(foundPages).toBe(expectedPages.length);
+    });
+
+    test("Then it should render the home link as highlighted", () => {
+      const expectedPage = "Home";
+      const expectedStyle = {
+        color: "white",
+      };
+      renderInRouter(<NavBar />);
+
+      const foundHomeLink = screen.getByRole("link", { name: expectedPage });
+
+      expect(foundHomeLink).toHaveStyle(expectedStyle);
+    });
+  });
+});
---
+++ src/setupTests.tsx
@@ -0,0 +1,16 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ReactNode, ReactElement } from "react";
+import { render } from "@testing-library/react";
+
+export const renderInRouter = (component: ReactElement) => {
+  const Bocata = ({ children }: { children: ReactNode }) => (
+    <BrowserRouter>{children}</BrowserRouter>
+  );
+
+  render(component, { wrapper: Bocata });
+};
