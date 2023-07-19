b6683f6325fded67391ba14aca11cbc3710c30a6
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-19 15:05:13 +0200

    created the HomePage component and added it to the routes

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Holder = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: red;
+`;
+
+const HomePage = () => {
+  return <Holder></Holder>;
+};
+
+export default HomePage;
