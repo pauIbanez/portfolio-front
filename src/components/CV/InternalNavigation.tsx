ad5014e6370ba1e7d22abe61b1c169315d219a53
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-20 13:19:40 +0200

    created the InternalNavigation

---
+++ src/components/CV/InternalNavigation.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import Colors from "../../data/style/Colors";
+
+const Holder = styled.div`
+  background-color: white;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.1), -4px -4px 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  border-radius: 15px;
+
+  display: flex;
+  flex-direction: column;
+
+  width: 165px;
+`;
+
+const Item = styled.button<{ $active: boolean }>`
+  border: none;
+  background: none;
+
+  width: 100%;
+  height: 50px;
+
+  color: ${Colors.textGray};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${(props) =>
+    props.$active
+      ? "background-color: " +
+        Colors.disabledMain +
+        "; color: " +
+        Colors.disabledTextGray
+      : ""}
+
+  &:hover {
+    background-color: ${Colors.disabledMain};
+    color: white;
+  }
+`;
+
+interface Props {
+  onClick: (scrollTo: string) => void;
+  items: {
+    name: string;
+    scrollTo: string;
+    active: boolean;
+  }[];
+}
+
+const InternalNavigation = ({ onClick, items }: Props) => {
+  const renderItems = items.map((item) => (
+    <Item
+      key={item.name}
+      onClick={() => {
+        onClick(item.scrollTo);
+      }}
+      $active={item.active}
+    >
+      {item.name}
+    </Item>
+  ));
+  return <Holder>{renderItems}</Holder>;
+};
+
+export default InternalNavigation;
