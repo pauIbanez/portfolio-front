47b862a982b1ab63a71252348a0f96f22e509f60
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-23 17:45:43 +0200

    added the CVPage to App

--- src/Pages/CVPage/CVPage.tsx
+++ src/Pages/CVPage/CVPage.tsx
@@ -1,7 +1,14 @@
 import { useRef } from "react";
+import ScrollContextProvider from "../../contexts/scrollContext/ScrollContextProvider";
 
 const CVPage = () => {
   const cvRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <ScrollContextProvider>
+      <p>hello</p>
+    </ScrollContextProvider>
+  );
 };
 
 export default CVPage;
