2e47f60c4c22afcef8a6c0f2d093c490327b7b3b
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-31 12:53:26 +0200

    prevented wheel scroll while autoScrolling and disabled auto currentActive while autoScrolling

--- src/contexts/scrollContext/ScrollContextProvider.tsx
+++ src/contexts/scrollContext/ScrollContextProvider.tsx
@@ -10,6 +10,7 @@ interface Props {
 const ScrollContextProvider = ({ children }: Props) => {
   const scrollItems = useRef<ScrollItem[]>([]);
   const [currentActive, setCurrentActive] = useState<string>("");
+  const [isAutoScrolling, setIsAutoScrolling] = useState<boolean>(false);
 
   const loadItem = (item: ScrollItem) => {
     scrollItems.current.push(item);
@@ -22,6 +23,8 @@ const ScrollContextProvider = ({ children }: Props) => {
       console.log("Ref not found: " + name);
       return;
     }
+    setIsAutoScrolling(true);
+    setCurrentActive(name);
     const rect = foundItem.ref.current?.getBoundingClientRect();
     window.scrollTo({
       top: (rect?.top || 0) + window.scrollY - (offsetY || 100),
@@ -30,6 +33,10 @@ const ScrollContextProvider = ({ children }: Props) => {
   };
 
   const onScroll = useCallback(() => {
+    if (isAutoScrolling) {
+      return;
+    }
+
     let closestItem: string = "";
     let closestItemHeight: number = 0;
 
@@ -45,13 +52,45 @@ const ScrollContextProvider = ({ children }: Props) => {
     });
 
     setCurrentActive(closestItem);
-  }, []);
+  }, [isAutoScrolling]);
+
+  const onScrollEnd = useCallback(() => {
+    if (isAutoScrolling) {
+      setIsAutoScrolling(false);
+    }
+  }, [isAutoScrolling]);
 
   useEffect(() => {
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
   }, [onScroll]);
+
+  useEffect(() => {
+    window.addEventListener("scrollend", onScrollEnd);
+
+    return () => window.removeEventListener("scrollend", onScrollEnd);
+  }, [onScrollEnd]);
+
+  //#region Preventing scroll
+
+  const preventScroll = useCallback(
+    (event: any) => {
+      if (isAutoScrolling) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    },
+    [isAutoScrolling]
+  );
+
+  useEffect(() => {
+    window.addEventListener("wheel", preventScroll, { passive: false });
+
+    return () => window.removeEventListener("wheel", preventScroll);
+  }, [preventScroll]);
+
+  //#endregion
 
   const getItems = (): ScrollItem[] => scrollItems.current;
 
