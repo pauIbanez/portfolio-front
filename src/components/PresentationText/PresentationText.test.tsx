649e7b3adea5fc24d42fd47e0bff29442558f79b
Pau Ibáñez <pauibanez2001@gmail.com>
2023-08-08 16:15:24 +0200

    added other test cases for presentationText

--- src/components/PresentationText/PresentationText.test.tsx
+++ src/components/PresentationText/PresentationText.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import PresentationText from "./PresentationText";
+import userEvent from "@testing-library/user-event";
 
 describe("Given the PresentationText component", () => {
   describe("When it's intanciated with a title, text and button data", () => {
@@ -24,6 +25,49 @@ describe("Given the PresentationText component", () => {
       expect(foundTitle).toBeInTheDocument();
       expect(foundText).toBeInTheDocument();
       expect(foundButton).toBeInTheDocument();
+    });
+
+    test("Then it should render title with font weight 700 and size 30px", () => {
+      const title = "title";
+      const expectedStyle = {
+        fontWeight: 700,
+        fontSize: "30px",
+      };
+
+      render(
+        <PresentationText
+          title={title}
+          text=""
+          button={{ content: <></>, onClick: () => null }}
+        />
+      );
+
+      const foundTitle = screen.getByRole("heading", { level: 2, name: title });
+
+      expect(foundTitle).toBeInTheDocument();
+      expect(foundTitle).toHaveStyle(expectedStyle);
+    });
+  });
+  describe("When it's intanciated and the button is pressed", () => {
+    test("Then it should call the passed onClick button function", () => {
+      const title = "title";
+      const text = "text test";
+      const buttonName = "Button content";
+
+      const buttonData = {
+        content: <p>{buttonName}</p>,
+        onClick: jest.fn(),
+      };
+
+      render(
+        <PresentationText title={title} text={text} button={buttonData} />
+      );
+
+      const foundButton = screen.getByRole("button", { name: buttonName });
+
+      userEvent.click(foundButton);
+
+      expect(buttonData.onClick).toHaveBeenCalled();
     });
   });
 });
