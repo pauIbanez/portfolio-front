4c4216d9e2963ccc73c12597c14ce6df8e6d5f50
Pau Ibáñez <pauibanez2001@gmail.com>
2023-08-12 17:20:43 +0200

    added missing test for the button padding component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -50,6 +50,7 @@ describe("Given the Button component", () => {
     test("Then it should render the a button with the passed styles", () => {
       const text = "test button";
       const style = {
+        padding: 10,
         fontSize: 11,
         fontWeight: 700,
         height: 13,
@@ -57,6 +58,7 @@ describe("Given the Button component", () => {
         width: 30,
       };
       const expectedStyle = {
+        padding: "0 10px 0 10px",
         fontSize: "11px",
         fontWeight: 700,
         height: "13px",
