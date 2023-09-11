f282959364f21f5b260a5655262113783a1e3129
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-11 09:08:22 +0200

    animated the interactive icon

--- src/components/projects/ProjectCard/ProjectCard.tsx
+++ src/components/projects/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,11 @@
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import ProjectCardInfo from "../../../Types/ProjectCardInfo";
 import Colors from "../../../data/style/Colors";
+
+interface HoverProp {
+  isHovering: boolean;
+}
 
 const CardHolder = styled.div`
   width: 440px;
@@ -35,24 +40,28 @@ const CardTitle = styled.h3`
   font-size: 35px;
 `;
 
-const CardInfo = styled.div`
+const CardInfo = styled.div<HoverProp>`
   position: absolute;
   left: 0;
   right: 0;
   bottom: 0;
-  height: 85px;
+  height: ${(props) => (props.isHovering ? "100%" : "85px")};
   display: flex;
   flex-direction: column;
   background-color: white;
   justify-content: space-between;
   padding: 15px 25px;
+  transition: all ease-in-out 0.1s;
 `;
 
-const CardInfoName = styled.h4`
+const CardInfoName = styled.h4<HoverProp>`
   margin: 0;
   color: black;
   font-weight: 700;
   font-size: 16px;
+
+  opacity: ${(props) => (props.isHovering ? "0" : "1")};
+  transition: all ease-in-out 0.1s;
 `;
 
 const CardInfoTagHolder = styled.div`
@@ -66,19 +75,33 @@ const MinimizedTag = styled.img`
   border-radius: 3px;
 `;
 
-const InteractiveIcon = styled.div`
+const InteractiveIcon = styled.div<HoverProp>`
   position: absolute;
   top: 18px;
   right: 18px;
   height: 30px;
-  width: 30px;
-  background-color: ${Colors.disabledMain};
+  width: ${(props) => (props.isHovering ? "100px" : "30px")};
+  background-color: ${(props) =>
+    props.isHovering ? Colors.main : Colors.disabledMain};
   color: white;
-  font-size: 20px;
+  font-size: 15px;
+  // font-weight: 700;
+  border-radius: 30px;
+  z-index: 10;
+
+  transition: all ease-in-out 0.1s;
+`;
+
+const InteractiveText = styled.p<HoverProp>`
   display flex;
   align-items: center;
   justify-content: center;
-  border-radius: 30px;
+  overflow: hidden;
+  height: 100%;
+  width: ${(props) => (props.isHovering ? "100%" : "30px")};
+  transition: all  0.1s ease-in-out 0.1s;
+  margin: 0;
+
 `;
 
 interface Props {
@@ -88,6 +111,10 @@ interface Props {
 const ProjectCard = ({
   cardInfo: { name, description, image, tags, isInteractive },
 }: Props) => {
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+  const isHoveringRef = useRef<boolean>(false);
+  const [interactiveContent, setInteractiveContent] = useState<string>("i");
+
   const renderTags = tags.map((tag) => (
     <MinimizedTag
       src={`./media/tags/${tag.icon}`}
@@ -95,10 +122,33 @@ const ProjectCard = ({
       key={tag.name}
     />
   ));
+
+  useEffect(() => {
+    isHoveringRef.current = isHovering;
+    if (isHovering) {
+      setTimeout(() => {
+        if (isHoveringRef.current) {
+          setInteractiveContent("interactive");
+        }
+      }, 100);
+    } else {
+      setInteractiveContent("i");
+    }
+  }, [isHovering]);
+
   return (
-    <CardHolder>
+    <CardHolder
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
       <CardPresentation>
-        {isInteractive && <InteractiveIcon>i</InteractiveIcon>}
+        {isInteractive && (
+          <InteractiveIcon isHovering={isHovering}>
+            <InteractiveText isHovering={isHovering}>
+              {interactiveContent}
+            </InteractiveText>
+          </InteractiveIcon>
+        )}
         <CardTitleHolder>
           <CardIcon
             alt={`${name} logo`}
@@ -109,8 +159,8 @@ const ProjectCard = ({
           <CardTitle>{name}</CardTitle>
         </CardTitleHolder>
       </CardPresentation>
-      <CardInfo>
-        <CardInfoName>{name}</CardInfoName>
+      <CardInfo isHovering={isHovering}>
+        <CardInfoName isHovering={isHovering}>{name}</CardInfoName>
         <CardInfoTagHolder>{renderTags}</CardInfoTagHolder>
       </CardInfo>
     </CardHolder>
