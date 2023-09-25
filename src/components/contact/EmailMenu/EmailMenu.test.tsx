82bcb2847ea2e1feaf471544fe56372defeb1d4e
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-25 18:07:18 +0200

    added test for visiblity when clicked

--- src/components/contact/EmailMenu/EmailMenu.test.tsx
+++ src/components/contact/EmailMenu/EmailMenu.test.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import { screen } from "@testing-library/react";
 import EmailMenu from "./EmailMenu";
 import { renderInRouter } from "../../../setupTests";
+import { act } from "react-dom/test-utils";
+import userEvent from "@testing-library/user-event";
 
 describe("Given the EmailMenu component", () => {
   describe("When it's instanciated", () => {
@@ -35,6 +38,40 @@ describe("Given the EmailMenu component", () => {
 
       expect(foundEmailLink).toBeInTheDocument();
       expect(foundEmailLink).not.toBeVisible();
+    });
+  });
+
+  describe("When it's instanciated and the emailButotn is clicked", () => {
+    test("Then it should render a coppy email button and a open email link, as visible", () => {
+      const expectedButton =
+        "email Contact.contactInfo.itemNames.email: pauibanez2001@gmail.com";
+      const expectedCopyButton = "Contact.contactInfo.itemValues.copyEmail";
+      const expectedOpenLink =
+        "Contact.contactInfo.itemValues.openEmail newscreen";
+
+      renderInRouter(<EmailMenu />);
+
+      const foundButton = screen.getByRole("button", {
+        name: expectedButton,
+      });
+
+      act(() => {
+        userEvent.click(foundButton);
+      });
+
+      const foundCopyButton = screen.getByRole("button", {
+        name: expectedCopyButton,
+      });
+
+      const foundEmailLink = screen.getByRole("link", {
+        name: expectedOpenLink,
+      });
+
+      expect(foundCopyButton).toBeInTheDocument();
+      expect(foundCopyButton).toBeVisible();
+
+      expect(foundEmailLink).toBeInTheDocument();
+      expect(foundEmailLink).toBeVisible();
     });
   });
 });
