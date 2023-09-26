22dd39c9b138f8db6239cbeda5113b06c722f023
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-26 10:52:49 +0200

    added game loop for simon and some styling

--- src/components/minigames/SimonGame/Simon/Simon.tsx
+++ src/components/minigames/SimonGame/Simon/Simon.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import GameSection from "../../GameSection/GameSection";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import SimonTileData from "../../../../Types/SimonTileData";
 import useEffectOnce from "../../../../hooks/useEffectOnce";
 import SimonTile from "../SimonTile/SimonTile";
 import Wait from "../../../../utils/Wait/Wait";
 import Button from "../../../Button/Button";
+import TiteledText from "../../../textComponents/TitledText/TiteledText";
+import Colors from "../../../../data/style/Colors";
 
 const GameHolder = styled.div`
   display: flex;
@@ -15,15 +17,33 @@ const GameHolder = styled.div`
   gap: 30px;
 `;
 
+const GameRow = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+`;
+
 const TileHolder = styled.div`
+  margin: 0 auto;
+  height: 350px;
+  width: 350px;
   display: flex;
   align-items: center;
   justify-items: center;
   flex-direction: column;
-  height: 350px;
-  width: 350px;
-  overflow: hidden;
   border-radius: 175px;
+  overflow: hidden;
+`;
+
+const TileBackground = styled.div`
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  height: 380px;
+  width: 380px;
+  background-color: ${Colors.backgroundGray};
+  border-radius: 190px;
+  border: 2px solid ${Colors.disabledMain};
 `;
 
 const TileRow = styled.div`
@@ -39,12 +59,21 @@ const ControlsHolder = styled.div`
   gap: 20px;
 `;
 
+const StatsHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+`;
+
 const Simon = () => {
   const [tiles, setTiles] = useState<SimonTileData[]>([]);
   const round = useRef<number>(0);
   const sequence = useRef<number[]>([]);
   const expectedSequence = useRef<number[]>([]);
   const [canClick, setCanClick] = useState<boolean>(false);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
   const [stats, setStats] = useState<{
     streak: number;
     maxStreak: number;
@@ -76,7 +105,6 @@ const Simon = () => {
 
   const calculateRound = useCallback(async () => {
     round.current++;
-
     const newValue = Math.floor(Math.random() * 4);
     sequence.current.push(newValue);
     expectedSequence.current = [...sequence.current];
@@ -84,12 +112,19 @@ const Simon = () => {
   }, [startRound]);
 
   const setupGame = useCallback(() => {
+    setIsPlaying(true);
     setStats((prevStats) => ({
       ...prevStats,
       streak: 0,
     }));
     setCanClick(false);
 
+    round.current = 0;
+    sequence.current = [];
+    calculateRound();
+  }, [calculateRound]);
+
+  useEffectOnce(() => {
     const newTiles: SimonTileData[] = [...Array(4)].map((_, index) => ({
       id: index,
       isShowing: false,
@@ -97,13 +132,6 @@ const Simon = () => {
     }));
 
     setTiles(newTiles);
-    round.current = 1;
-    sequence.current = [];
-    calculateRound();
-  }, [calculateRound]);
-
-  useEffectOnce(() => {
-    setupGame();
     setStats((prevStats) => ({
       ...prevStats,
       maxStreak: localStorage.getItem(`simonStreak`)
@@ -112,20 +140,39 @@ const Simon = () => {
     }));
   });
 
-  const onTileClick = async (value: number) => {
-    console.log("clicked", value);
+  const onTileClick = useCallback(
+    async (value: number) => {
+      console.log("clicked", value);
 
-    if (expectedSequence.current[0] === value) {
-      expectedSequence.current.shift();
-      if (expectedSequence.current.length === 0) {
+      if (expectedSequence.current[0] === value) {
+        expectedSequence.current.shift();
+        if (expectedSequence.current.length === 0) {
+          setCanClick(false);
+          setStats((prevStats) => ({
+            ...prevStats,
+            streak: prevStats.streak++,
+          }));
+          await Wait(1500);
+          calculateRound();
+        }
+      } else {
+        console.log("FAILED");
         setCanClick(false);
-        await Wait(1500);
-        calculateRound();
+        setIsPlaying(false);
+        let newMaxStreak = 0;
+        const prevMaxStreak = localStorage.getItem(`simonStreak`);
+        if (!prevMaxStreak || Number.parseInt(prevMaxStreak) < stats.streak) {
+          newMaxStreak = stats.streak;
+          localStorage.setItem(`simonStreak`, stats.streak.toString());
+        }
+        setStats({
+          streak: 0,
+          maxStreak: newMaxStreak === 0 ? stats.maxStreak : newMaxStreak,
+        });
       }
-    } else {
-      console.log("FAILED");
-    }
-  };
+    },
+    [calculateRound, stats]
+  );
 
   const renderTiles = tiles.map((tile) => (
     <SimonTile
@@ -146,6 +193,17 @@ const Simon = () => {
     padding: 0,
   };
 
+  const statStyle = {
+    title: {
+      size: 15,
+    },
+    text: {
+      size: 13,
+      color: Colors.lightTextGray,
+    },
+    gap: 3,
+  };
+
   return (
     <GameSection title="Simon">
       <>
@@ -153,17 +211,33 @@ const Simon = () => {
           The goal of simon is to repeat the sequence for as long as you can!
         </p>
         <GameHolder>
-          <TileHolder>
-            {renderTiles[0]}
-            <TileRow>
-              {renderTiles[1]}
-              {renderTiles[3]}
-            </TileRow>
-            {renderTiles[2]}
-          </TileHolder>
+          <GameRow>
+            <TileBackground>
+              <TileHolder>
+                {renderTiles[0]}
+                <TileRow>
+                  {renderTiles[1]}
+                  {renderTiles[3]}
+                </TileRow>
+                {renderTiles[2]}
+              </TileHolder>
+            </TileBackground>
+            <StatsHolder>
+              <TiteledText
+                title="Streak"
+                text={stats.streak.toString()}
+                styleObject={statStyle}
+              />
+              <TiteledText
+                title="Best Streak"
+                text={stats.maxStreak.toString()}
+                styleObject={statStyle}
+              />
+            </StatsHolder>
+          </GameRow>
           <ControlsHolder>
             <Button styleObject={buttonStyle} onClick={setupGame}>
-              Restart Game
+              {!isPlaying ? "Play Simon" : "Restart Game"}
             </Button>
           </ControlsHolder>
         </GameHolder>
