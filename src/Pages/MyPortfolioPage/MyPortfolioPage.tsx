3353bd19968231aef700bc6be90c6ec0aca38f0c
Pau Ibáñez <pauibanez2001@gmail.com>
2023-10-01 12:16:01 +0200

    added most of myPortfolioPage content

--- src/Pages/MyPortfolioPage/MyPortfolioPage.tsx
+++ src/Pages/MyPortfolioPage/MyPortfolioPage.tsx
@@ -74,6 +74,20 @@ const LinkSectionHolder = styled.div`
   justify-content: space-around;
 `;
 
+const ImageHolder = styled.div`
+  display: flex;
+  justify-content: center;
+
+  img {
+    object-fit: contain;
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.1),
+      -4px -4px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 15px;
+
+    border: 2px solid ${Colors.disabledMain};
+  }
+`;
+
 const MyPortfolioPage = () => {
   const { t } = useTranslation();
 
@@ -183,24 +197,6 @@ const MyPortfolioPage = () => {
         </Section>
         <Section>
           <TiteledText
-            title={"Design"}
-            text={
-              "This website is almost completley of my own design, all except my home page, that was heavely inspired by a posted template design from a UX/UI designer. Unfortunatley I can't find the original image. If it's your design please contact me so I can add the propper credits!"
-            }
-            styleObject={{ title: { heading: 4 } }}
-          />
-        </Section>
-        <Section>
-          <TiteledText
-            title={"Coding"}
-            text={
-              "As you've seen in the project tags, this website is built with react and typescript, my favorite platforms for constructiong web elements! I've focused on writing clean and mantainable code, adhering to a DRY mentality, but not going so obverboard that I restrict myself on design changes in the future. "
-            }
-            styleObject={{ title: { heading: 4 } }}
-          />
-        </Section>
-        <Section>
-          <TiteledText
             title={"Enviroments"}
             text={
               "This website has been worked on using the react-embeded webpack solution for the front and using nodemon for the back.\n My production enviroment has been implemented using <&>Netlify<&>'s hosting service for the front and <&>Render<&> for the back."
@@ -245,6 +241,44 @@ const MyPortfolioPage = () => {
         </Section>
         <Section>
           <TiteledText
+            title={"Design"}
+            text={
+              "This website is almost completley of my own design, all except my home page, that was heavely inspired by a posted template design from a UX/UI designer. Unfortunatley I can't find the original image. If it's your design please contact me so I can add the propper credits!"
+            }
+            styleObject={{ title: { heading: 4 } }}
+          />
+        </Section>
+        <Section>
+          <TiteledText
+            title={"Languages"}
+            text={
+              "I've implemented i18n with i18n-react to be able to present my portfolio en english and spanish, this was my 2nd time ever using i18n, with the first one being at Blikk, using it with Flutter, so kind of the first time. I've learned that It's super easy to use, but translating 800 different texts takes a lot of time 😩. \n \n You can find the language toggle on the top left in case you missed it."
+            }
+            styleObject={{ title: { heading: 4 } }}
+          />
+        </Section>
+        <Section>
+          <TiteledText
+            title={"Coding"}
+            text={
+              "As you've seen in the project tags, this website is built with react and typescript, my favorite platforms for constructiong web elements! I've focused on writing clean and mantainable code, adhering to a DRY mentality, but not going so obverboard that I restrict myself on design changes in the future. \n I strongly invite you to visit the GitHub page linked above and check out some of my code. \n \n This time, for the back-end, I've used my own template for the server base, even though I don't need any users for this website it was still easier to use that template and strip the user parts than to construct a new server yet again!"
+            }
+          />
+          <LinkSection>
+            <TiteledText
+              title={""}
+              text={"You can find my emplate in the projects page or here:"}
+            />
+            <FeatureHolder>
+              <Bullet />
+              <Link to={"/project/backendTemplate"} target="_blank">
+                Backend template
+              </Link>
+            </FeatureHolder>
+          </LinkSection>
+        </Section>
+        <Section>
+          <TiteledText
             title={"Testing"}
             text={
               "Since the very begining of the project I've been testing most of my code. Even though the <&>80% coverage<&> is standard I usually like to strive for that <&>100%<&>. Who does not get dopamine sseing all the geen 100's haha. Anyway, most of my code has been thurougly tested, but I have encountered some problemts, mostly regarding the scroling functionalities, because aparently <&>jest<&> and <&>testing-library<&> don't have scroll testing support yet, since they are node-based testing enviroments."
@@ -252,12 +286,35 @@ const MyPortfolioPage = () => {
             styleObject={{ title: { heading: 4 } }}
           />
           <TiteledText
-            title={"SonarQube"}
+            title={"Jest and Testing-library"}
             text={
-              "I like to use SonarQube and Sonar Scanner as my static analizer to keep a more acturate track of my coverage, possible bugs or code smells. This is why I've included my sonar-project.properties file in the repo, allowing a propper implementation in any terminal in a cople clicks."
+              "I use jest with the react/testing-library when testing my react apps. I find it easy to use and super complete for what it was designed to do. Here are my latests jest coverage metrics by file:"
             }
             styleObject={{ title: { heading: 5, size: 16 }, gap: 5 }}
           />
+          <ImageHolder>
+            <img
+              src="/media/portfolio/test.png"
+              alt="jest coverage terminal"
+              height={1552}
+              width={770}
+            />
+          </ImageHolder>
+          <TiteledText
+            title={"SonarQube"}
+            text={
+              "I like to use SonarQube and Sonar Scanner as my static analizer to keep a more acturate track of my coverage, possible bugs or code smells. This is why I've included my sonar-project.properties file in the repo, allowing a propper implementation in any terminal in a cople clicks. Tis is my last analized summary:"
+            }
+            styleObject={{ title: { heading: 5, size: 16 }, gap: 5 }}
+          />
+          <ImageHolder>
+            <img
+              src="/media/portfolio/sonarQube.png"
+              alt="SonarQube summary - 0 Bugs | 0 Code Smells | 0 Vulnerabilities | 0 Security Hotspots | 80.7% Coverage | 1.8% Duplications on 6 blocks"
+              height={400}
+              width={770}
+            />
+          </ImageHolder>
         </Section>
       </ContentHolder>
     </Holder>
