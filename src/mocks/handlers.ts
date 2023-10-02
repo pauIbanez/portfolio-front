15be0ab2f0812e8c6947acb2e3fcb5ae257e739b
Pau Ibáñez <pauibanez2001@gmail.com>
2023-10-02 16:26:55 +0200

    added msw with the server and handlers

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,28 @@
+import { rest } from "msw";
+
+const handlers = [
+  rest.post(
+    `${process.env.REACT_APP_BACKEND_URL}/newMessage`,
+    async (req, res, ctx) => {
+      if ((await req.json()).mockOk) {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            message: "Message received!",
+          })
+        );
+      } else {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            code: 400,
+            error: true,
+            message: "Invalid message",
+          })
+        );
+      }
+    }
+  ),
+];
+
+export default handlers;
