a11af2568ef6390f30a6664b5b2879d5732df0bd
Pau Ibáñez <pauibanez2001@gmail.com>
2023-10-02 17:29:45 +0200

    tested the noMobilePage

---
+++ src/Pages/NoMobilePage/NoMobilePage.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import NoMobilePage from "./NoMobilePage";
+import userEvent from "@testing-library/user-event";
+
+describe("Given the NoMobile Page", () => {
+  describe("When it's instanciated", () => {
+    test("Then it should render the disclaimer", () => {
+      const expectedTitle = "No mobile";
+      const expectedText = "mobile";
+
+      render(<NoMobilePage onClick={() => null} />);
+
+      const foundTitle = screen.getByRole("heading", {
+        level: 3,
+        name: expectedTitle,
+      });
+      const foundText = screen.getByText(expectedText);
+
+      expect(foundTitle).toBeInTheDocument();
+      expect(foundText).toBeInTheDocument();
+    });
+
+    test("Then it should render a button", () => {
+      const expectedButtonName = "Visit anyway";
+
+      render(<NoMobilePage onClick={() => null} />);
+
+      const foundButton = screen.getByRole("button", {
+        name: expectedButtonName,
+      });
+
+      expect(foundButton).toBeInTheDocument();
+    });
+  });
+  describe("When the button is clcked", () => {
+    test("Then it should call the passed onClick func", () => {
+      const expectedButtonName = "Visit anyway";
+      const onClick = jest.fn();
+
+      render(<NoMobilePage onClick={onClick} />);
+
+      const foundButton = screen.getByRole("button", {
+        name: expectedButtonName,
+      });
+
+      userEvent.click(foundButton);
+
+      expect(onClick).toHaveBeenCalled();
+    });
+  });
+});
