1ead70604d3129d5c5a78cf2be29d728342f5070
Pau Ibáñez <pauibanez2001@gmail.com>
2023-10-09 17:23:20 +0200

    applied portfolio translations

--- src/Pages/MyPortfolioPage/MyPortfolioPage.tsx
+++ src/Pages/MyPortfolioPage/MyPortfolioPage.tsx
@@ -104,22 +104,20 @@ const MyPortfolioPage = () => {
       <ContentHolder>
         <Section>
           <TiteledText
-            title={"Project management"}
-            text={
-              "In order to keep me organized, I've used varius task management and design tools. Other than the obvious version control, git. \n \n For this project I've used <&>GitHub<&> for the version control hosting, <&>Monday<&>, a task management tool and <&>Figma<&> for the website design. Check out these relevant links for an insignt on my project management while building this website."
-            }
+            title={t("MyPortfolio.management.title")}
+            text={t("MyPortfolio.management.text")}
             styleObject={{ title: { heading: 4 } }}
           />
           <LinkSectionHolder>
             <LinkSection>
-              GitHub:
+              {t("MyPortfolio.management.linkSections.0.name")}
               <FeatureHolder>
                 <Bullet />
                 <Link
                   to={"https://github.com/pauIbanez/portfolio-front"}
                   target="_blank"
                 >
-                  Front-End
+                  {t("MyPortfolio.management.linkSections.0.links.0")}
                 </Link>
               </FeatureHolder>
               <FeatureHolder>
@@ -128,12 +126,12 @@ const MyPortfolioPage = () => {
                   to={"https://github.com/pauIbanez/portfolio-back"}
                   target="_blank"
                 >
-                  Back-End
+                  {t("MyPortfolio.management.linkSections.0.links.1")}
                 </Link>
               </FeatureHolder>
             </LinkSection>
             <LinkSection>
-              My monday boards:
+              {t("MyPortfolio.management.linkSections.1.name")}
               <FeatureHolder>
                 <Bullet />
                 <Link
@@ -142,7 +140,7 @@ const MyPortfolioPage = () => {
                   }
                   target="_blank"
                 >
-                  Project management
+                  {t("MyPortfolio.management.linkSections.1.links.0")}
                 </Link>
               </FeatureHolder>
               <FeatureHolder>
@@ -153,7 +151,7 @@ const MyPortfolioPage = () => {
                   }
                   target="_blank"
                 >
-                  Font-End
+                  {t("MyPortfolio.management.linkSections.1.links.1")}
                 </Link>
               </FeatureHolder>
               <FeatureHolder>
@@ -164,12 +162,12 @@ const MyPortfolioPage = () => {
                   }
                   target="_blank"
                 >
-                  Back-End
+                  {t("MyPortfolio.management.linkSections.1.links.2")}
                 </Link>
               </FeatureHolder>
             </LinkSection>
             <LinkSection>
-              Figma:
+              {t("MyPortfolio.management.linkSections.2.name")}
               <FeatureHolder>
                 <Bullet />
                 <Link
@@ -178,7 +176,7 @@ const MyPortfolioPage = () => {
                   }
                   target="_blank"
                 >
-                  Figma design
+                  {t("MyPortfolio.management.linkSections.2.links.0")}
                 </Link>
               </FeatureHolder>
               <FeatureHolder>
@@ -189,7 +187,7 @@ const MyPortfolioPage = () => {
                   }
                   target="_blank"
                 >
-                  Figma prototype
+                  {t("MyPortfolio.management.linkSections.2.links.1")}
                 </Link>
               </FeatureHolder>
             </LinkSection>
@@ -197,33 +195,29 @@ const MyPortfolioPage = () => {
         </Section>
         <Section>
           <TiteledText
-            title={"Enviroments"}
-            text={
-              "This website has been worked on using the react-embeded webpack solution for the front and using nodemon for the back.\n My production enviroment has been implemented using <&>Netlify<&>'s hosting service for the front and <&>Render<&> for the back."
-            }
+            title={t("MyPortfolio.enviroments.title")}
+            text={t("MyPortfolio.enviroments.text")}
             styleObject={{ title: { heading: 4 } }}
           />
           <LinkSection>
-            Enviroment providers:
+            {t("MyPortfolio.enviroments.linkSections.0.name")}
             <FeatureHolder>
               <Bullet />
               <Link to={"https://www.netlify.com/"} target="_blank">
-                Netlify Front-End
+                {t("MyPortfolio.enviroments.linkSections.0.links.0")}
               </Link>
             </FeatureHolder>
             <FeatureHolder>
               <Bullet />
               <Link to={"https://render.com/"} target="_blank">
-                Render Back-End
+                {t("MyPortfolio.enviroments.linkSections.0.links.1")}
               </Link>
             </FeatureHolder>
           </LinkSection>
           <LinkSection>
             <TiteledText
               title={""}
-              text={
-                "I've also used both Netlify's actions aswell as my own <&>Audit<&> action which performs some checks before allowing me to push to production. The most important chekcs it perofrms are the ESLint validation (code formating) and the Testing validation which ensures all tests pass. \n \n Check the workflow out:"
-              }
+              text={t("MyPortfolio.enviroments.text2")}
               styleObject={{ title: { heading: 4 } }}
             />
             <FeatureHolder>
@@ -234,83 +228,72 @@ const MyPortfolioPage = () => {
                 }
                 target="_blank"
               >
-                Audit action
+                {t("MyPortfolio.enviroments.linkSections.1.links.0")}
               </Link>
             </FeatureHolder>
           </LinkSection>
         </Section>
         <Section>
           <TiteledText
-            title={"Design"}
-            text={
-              "This website is almost completley of my own design, all except my home page, that was heavely inspired by a posted template design from a UX/UI designer. Unfortunatley I can't find the original image. If it's your design please contact me so I can add the propper credits!"
-            }
+            title={t("MyPortfolio.design.title")}
+            text={t("MyPortfolio.design.text")}
             styleObject={{ title: { heading: 4 } }}
           />
         </Section>
         <Section>
           <TiteledText
-            title={"Languages"}
-            text={
-              "I've implemented i18n with i18n-react to be able to present my portfolio en english and spanish, this was my 2nd time ever using i18n, with the first one being at Blikk, using it with Flutter, so kind of the first time. I've learned that It's super easy to use, but translating 800 different texts takes a lot of time 😩. \n \n You can find the language toggle on the top left in case you missed it."
-            }
+            title={t("MyPortfolio.languages.title")}
+            text={t("MyPortfolio.languages.text")}
             styleObject={{ title: { heading: 4 } }}
           />
         </Section>
         <Section>
           <TiteledText
-            title={"Coding"}
-            text={
-              "As you've seen in the project tags, this website is built with react and typescript, my favorite platforms for constructiong web elements! I've focused on writing clean and mantainable code, adhering to a DRY mentality, but not going so obverboard that I restrict myself on design changes in the future. \n I strongly invite you to visit the GitHub page linked above and check out some of my code. \n \n This time, for the back-end, I've used my own template for the server base, even though I don't need any users for this website it was still easier to use that template and strip the user parts than to construct a new server yet again!"
-            }
+            title={t("MyPortfolio.coding.title")}
+            text={t("MyPortfolio.coding.text")}
+            styleObject={{ title: { heading: 4 } }}
           />
           <LinkSection>
             <TiteledText
               title={""}
-              text={"You can find my emplate in the projects page or here:"}
+              text={t("MyPortfolio.coding.linkSection.name")}
             />
             <FeatureHolder>
               <Bullet />
               <Link to={"/project/backendTemplate"} target="_blank">
-                Backend template
+                {t("MyPortfolio.coding.linkSection.link")}
               </Link>
             </FeatureHolder>
           </LinkSection>
         </Section>
         <Section>
           <TiteledText
-            title={"Testing"}
-            text={
-              "Since the very begining of the project I've been testing most of my code. Even though the <&>80% coverage<&> is standard I usually like to strive for that <&>100%<&>. Who does not get dopamine sseing all the geen 100's haha. Anyway, most of my code has been thurougly tested, but I have encountered some problemts, mostly regarding the scroling functionalities, because aparently <&>jest<&> and <&>testing-library<&> don't have scroll testing support yet, since they are node-based testing enviroments."
-            }
+            title={t("MyPortfolio.testing.title")}
+            text={t("MyPortfolio.testing.text")}
             styleObject={{ title: { heading: 4 } }}
           />
           <TiteledText
-            title={"SonarQube"}
-            text={
-              "I like to use SonarQube and Sonar Scanner as my static analizer to keep a more acturate track of my coverage, possible bugs or code smells. This is why I've included my sonar-project.properties file in the repo, allowing a propper implementation in any terminal in a cople clicks. Tis is my last analized summary:"
-            }
+            title={t("MyPortfolio.testing.sections.0.name")}
+            text={t("MyPortfolio.testing.sections.0.text")}
             styleObject={{ title: { heading: 5, size: 16 }, gap: 5 }}
           />
           <ImageHolder>
             <img
               src="/media/portfolio/sonarQube.png"
-              alt="SonarQube summary - 0 Bugs | 0 Code Smells | 0 Vulnerabilities | 0 Security Hotspots | 80.7% Coverage | 1.8% Duplications on 6 blocks"
+              alt={t("MyPortfolio.testing.sections.0.image")}
               height={400}
               width={770}
             />
           </ImageHolder>
           <TiteledText
-            title={"Jest and Testing-library"}
-            text={
-              "I use jest with the react/testing-library when testing my react apps. I find it easy to use and super complete for what it was designed to do. Here are my latests jest coverage metrics by file:"
-            }
+            title={t("MyPortfolio.testing.sections.1.name")}
+            text={t("MyPortfolio.testing.sections.1.text")}
             styleObject={{ title: { heading: 5, size: 16 }, gap: 5 }}
           />
           <ImageHolder>
             <img
               src="/media/portfolio/test.png"
-              alt="jest coverage terminal"
+              alt={t("MyPortfolio.testing.sections.1.image")}
               height={2193}
               width={770}
             />
